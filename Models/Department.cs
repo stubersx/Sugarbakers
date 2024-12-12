8fe4dd33294c036f51753ab0563a1d1e8ef434b4
stubersx <flows-snips-0u@icloud.com>
2024-12-12 16:36:32 -0500

    Add project files.

---
+++ Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sugarbakers.Models;
+
+public partial class Department
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Location { get; set; }
+
+    public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+}
---
+++ Models/Zipcode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sugarbakers.Models;
+
+public partial class Zipcode
+{
+    public string Zipcode1 { get; set; } = null!;
+
+    public string City { get; set; } = null!;
+
+    public string State { get; set; } = null!;
+
+    public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+}
